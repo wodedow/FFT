71ffc484d9cad7561aa321633780fa78bb1dc9a7
wodedow <wodehuangne@gmail.com>
2020-05-19 18:36:52 +0800

    添加项目文件。

---
+++ FFT.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+typedef complex<double> doublec;
+const double pi = 3.1415926;
+
+struct Array {
+	double* elem;
+	int length;
+};
+struct Arrayc {
+	doublec* elem;
+	int length;
+};
+bool Init_Array(Array& array,int length) {
+	array.elem = new double[length];
+	array.length = length;
+	return true;
+}
+bool Init_Arrayc(Arrayc& array, int length) {
+	array.elem = new doublec[length];
+	array.length = length;
+	return true;
+}
+
+bool FFT(Arrayc& array) {
+	int m = (int)log2(array.length);
+	if (m == 0)
+		return true;
+	int n = array.length / 2;
+
+	Arrayc arrayx;
+	Arrayc arrayy;
+	Init_Arrayc(arrayx, n);
+	Init_Arrayc(arrayy, n);
+	for (int i = 0; i < n; i++) {
+		arrayx.elem[i] = array.elem[2 * i];
+		arrayy.elem[i] = array.elem[2 * i + 1];
+	}
+
+	FFT(arrayx);
+	cout << "xTimes++" << endl;
+	FFT(arrayy);
+	cout << "yTimes++" << endl;
+
+	for (int k = 0; k < n; k++) {
+		doublec gk(0.0);
+		doublec hk(0.0);
+		for (int j = 0; j < n; j++) {
+			doublec ck(cos(-2 * pi * j*k / n), sin(-2 * pi * j*k / n));
+			gk += arrayx.elem[j] * ck;
+			hk += arrayy.elem[j] * ck;
+		}
+		//cout << "(" << gk.real() << ", " << gk.imag() << ")  " << endl;
+		//cout << "(" << hk.real() << ", " << hk.imag() << ")  " << endl;
+		doublec dk(cos(-pi * k / n), sin(-pi * k / n));
+		array.elem[k] = gk + dk * hk;
+		array.elem[k + n] = gk - dk * hk;
+	}
+	cout << "Times++" << endl;
+
+	return true;
+}
+
+int main() {
+	Arrayc xn;
+	int size = 8;
+	Init_Arrayc(xn, size);
+	for (int i = 0; i < size; i++) {
+		xn.elem[i] = (i);
+	}
+
+	FFT(xn);
+
+	for (int i = 0; i < xn.length; i++) {
+		cout << "(" << xn.elem[i].real() << ", " << xn.elem[i].imag() << ")  ";
+	}
+
+	return 1;
+}
---
+++ FFT2.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <complex>
+#include <math.h>
+
+using namespace std;
+
+const int M = 8;
+const int N = 16;
+
+struct Matrix {
+	double** elem;
+	int rows;
+	int cols;
+};
+struct Array {
+	double* elem;
+	int length;
+};
+struct Matrixc {
+	complex<double>** elem;
+	int rows;
+	int cols;
+};
+struct Arrayc {
+	complex<double>* elem;
+	int length;
+};
+
+int* swap(int j, int m) {
+	int* a = new int[m];
+	for (int i = 0; i < m; i++) {
+		a[i] = 0;
+	}
+
+	for (int i = m - 1; i >= 0; i--) {
+		a[i] = j % 2;
+		j = j / 2;
+		if (j == 0)
+			break;
+	}
+	return a;
+}
+
+Matrixc Translate(Matrixc mat) {
+	Matrixc matr;
+	matr.cols = mat.rows;
+	matr.rows = mat.cols;
+	matr.elem = new complex<double> * [mat.cols];
+	for (int i = 0; i < mat.cols; i++) {
+		matr.elem[i] = new complex<double>[mat.rows];
+	}
+
+	for (int i = 0; i < matr.rows; i++) {
+		for (int j = 0; j < matr.cols; j++) {
+			matr.elem[i][j] = mat.elem[j][i];
+		}
+	}
+
+	return matr;
+}
+
+template<typename T> complex<double>* FFT(T& arr) {
+	
+}
+
+Matrixc FFT2(Matrix& img) {
+	Matrixc imgg;
+	imgg.rows = img.rows;
+	imgg.cols = img.cols;
+
+	for (int i = 0; i < M; i++) {
+		imgg.elem[i]=FFT<double*>(img.elem[i]);
+	}
+	Matrixc imggg;
+	imggg = Translate(imgg);
+	Matrixc imgggg;
+	imgggg.rows = imggg.rows;
+	imgggg.cols = imggg.cols;
+
+	for (int j = 0; j < N; j++) {
+		imgggg.elem[j] = FFT<complex<double>*>(imggg.elem[j]);
+	}
+	imgg = Translate(imgggg);
+
+	memset(&imggg, 0, sizeof(imggg));
+	memset(&imgggg, 0, sizeof(imgggg));
+	return imgg;
+}
+
+//int main() {
+//	Matrix Img;
+//	Img.rows = M;
+//	Img.cols = N;
+//	Matrixc Img_out;
+//	Img_out = FFT2(Img);
+//
+//	return 1;
+//}
